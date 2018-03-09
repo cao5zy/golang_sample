85be2dfea829aa528c76436f2a888954d88e425e
Ubuntu <caozon@dev.02pz4bqcczde1igra14ffnabtb.hx.internal.cloudapp.net>
2018-03-09 02:34:48 +0000

    add lib1

---
+++ lib1/file1.go
@@ -0,0 +1,5 @@
+package main
+
+type Addr struct {
+     id int
+} 
---
+++ lib1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+       "fmt"
+)
+
+func main() {
+     n := Addr { id: 3 }
+     fmt.Println(n.id)
+}
+
