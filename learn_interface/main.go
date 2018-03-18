1b70f3746a04dbb11641fe396aa4ba159be3811a
Ubuntu <caozon@dev.02pz4bqcczde1igra14ffnabtb.hx.internal.cloudapp.net>
2018-03-18 12:18:59 +0000

    can run with interface

--- learn_interface/main.go
+++ learn_interface/main.go
@@ -1,15 +1,36 @@
 package main
 
 import "fmt"
+
 // this package aims to show how to convert interface to concrete types
 
-func main(){
-     data := make(map[string]interface{})
+func main() {
+	data := make(map[string]interface{})
 
-     data["a"] = 1
-     data["b"] = "b"
-     data["c"] = 0.8
+	data["a"] = 1
+	data["b"] = "b"
+	data["c"] = 0.8
 
-     fmt.Println(data["a"].(int) + 1) //convert interface{} to int
-     fmt.Println(data["b"].(string) + "oo") //convert interface{} to string
+	fmt.Println(data["a"].(int) + 1)       //convert interface{} to int
+	fmt.Println(data["b"].(string) + "oo") //convert interface{} to string
+
+	dog := Dog{ID: "2323223"}
+
+	WhoCanWalk(dog)
+}
+
+type Animal interface {
+	Walk()
+}
+
+type Dog struct {
+	ID string
+}
+
+func (d Dog) Walk() {
+	fmt.Println("dog can walk", d.ID)
+}
+
+func WhoCanWalk(animal Animal) {
+	animal.Walk()
 }
